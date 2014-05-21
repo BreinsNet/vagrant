3ceec16e4eab84bcd758764711c6a94d628a0f66
Juan Breinlinger <juan.brein@breins.net>
2014-05-21 15:39:25 +0100

    better run puppet scripts

--- bootstrap/common/run_puppet_agent.sh
+++ bootstrap/common/run_puppet_agent.sh
@@ -9,4 +9,24 @@ if [ ! -z $2 ]; then
   ENVIRONMENT="--environment $2"
 fi
 
+echo "INFO: running puppet agent on environment $2 , puppet master is: $1"
+
+if [[ $(id -u) -eq 0 ]];then
+  LOG_FILE=/var/log/bootstrap.log
+else
+  LOG_FILE=/vagrant/logs/bootstrap.log
+fi
+
+# Script error handling and output redirect
+exec >> $LOG_FILE                    # stdout to log file
+exec 2>&1                            # stderr to log file
+set -x
+
 puppet agent -t --server $1 $ENVIRONMENT
+
+if [[ $? -ne 4 ]]; then
+  exit 1
+else
+  exit 0
+fi
+
--- bootstrap/common/run_puppet_apply.sh
+++ bootstrap/common/run_puppet_apply.sh
@@ -5,11 +5,23 @@ if [ -z $1 ]; then
   exit 1
 fi
 
+echo "INFO: running puppet apply on environment $1"
+
+if [[ $(id -u) -eq 0 ]];then
+  LOG_FILE=/var/log/bootstrap.log
+else
+  LOG_FILE=/vagrant/logs/bootstrap.log
+fi
+
+# Script error handling and output redirect
+exec >> $LOG_FILE                    # stdout to log file
+exec 2>&1                            # stderr to log file
+set -x
+
 if which rvm > /dev/null 2>&1; then
   source /etc/profile.d/rvm.sh
   rvm use system
 fi
-
 
 ENVIRONMENT=$1
 
@@ -18,3 +30,9 @@ puppet apply \
   --modulepath=/etc/puppet/environments/$ENVIRONMENT/modules:/etc/puppet/environments/$ENVIRONMENT/custom \
   --hiera_config=/etc/puppet/hiera.yaml \
   /vagrant/manifests/default.pp
+
+if [[ $? -ne 4 ]]; then
+  exit 1
+else
+  exit 0
+fi
